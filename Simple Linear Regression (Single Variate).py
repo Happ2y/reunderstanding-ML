4ccb6782cde4403db23df8675975b0f928bdf4c5
Swaraj Kumar Chaudhary <swarajkumarchaudhary1729@gmail.com>
2022-05-28 23:04:18 +0530

    [ADD] Simple Linear Regression (Single Variate).py, LeastSquareDataSet.csv

---
+++ Simple Linear Regression (Single Variate).py
@@ -0,0 +1,43 @@
+# import dependencies
+import math
+import numpy
+import pandas
+from matplotlib import pyplot as plot
+
+if __name__ == "__main__":
+    # read csv file & separate dependent & independent values
+    df = pandas.read_csv("LeastSquareDataSet.csv")
+    X = df["x"]  # independent
+    Y = df["y"]  # dependent
+    print(df)
+
+    # calculate mean values
+    X_mean = numpy.mean(X)
+    Y_mean = numpy.mean(Y)
+
+    # calculate slope & intercept
+    Sxx = Sxy = 0
+    for i in range(len(df)):
+        Sxx += math.pow((X[i] - X_mean), 2)
+        Sxy += (X[i] - X_mean) * (Y[i] - Y_mean)
+
+    m = Sxy/Sxx
+    c = Y_mean - (m * X_mean)
+
+    # find prediction line
+    Y_prediction = m * X + c
+    print(f"Predicted Line : Yi = {m} Xi + {c}")
+    print("Predicted corresponding y values -")
+    print(Y_prediction)
+
+    # calculate error using RMSE
+    RMSE = 0
+    for i in range(len(df)):
+        RMSE += math.pow((Y[i] - Y_prediction[i]), 2)
+    RMSE = math.sqrt(RMSE/len(df))
+    print(f"Error : {RMSE}")
+
+    # plot actual & predicted values
+    plot.scatter(X, Y)  # actual
+    plot.scatter(X, Y_prediction, color="red")  # predicted
+    plot.show()
